f3e260a79829833bf5a7e9a1f486f9aa0e4af3b4
michaljaszczuk <michaljaszczuk@gmail.com>
2021-03-28 19:42:45 +0200

    added Employee.cs, EmployeeManager.cs

---
+++ CompanyManager/CompanyManager/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CompanyManager
+{
+    abstract class Employee
+    {
+        protected int ID { get; set; }
+        protected string Name { get; set; }
+        protected string Position { get; set; }
+        protected bool IsEmployed { get; set; }  
+        public abstract void CalculateSalary();
+        
+    }
+}
---
+++ CompanyManager/CompanyManager/EmployeeManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CompanyManager
+{
+    abstract class EmployeeManager
+    {
+        public abstract void SalaryRise();
+        public void HireEmployee()
+        { }
+        public void FireEmployee()
+        { }
+        
+    }
+}
