4283c5cf768d9d215bccd0d488f0a2b94344236b
michaljaszczuk <michaljaszczuk@gmail.com>
2021-04-28 18:34:33 +0200

    added CalculateSalary() implementations

--- CompanyManager/CompanyManager/Employee.cs
+++ CompanyManager/CompanyManager/Employee.cs
@@ -9,16 +9,21 @@ namespace CompanyManager
         protected int ID { get; set; }
         protected string Name { get; set; }
         protected string Position { get; set; }
-        protected bool IsEmployed { get; set; }  
+        protected bool IsEmployed { get; set; }
+
+        protected int Salary { get; set; }
         public abstract void CalculateSalary();
         
+
     }
 
     class HourlyEmployee : Employee
     {
         public override void CalculateSalary()
         {
-            throw new NotImplementedException();
+            const double hourly = 14.70;
+            double salaryAnnualy = 2 * hourly + 1000;
+            
         }
     }
 
@@ -26,7 +31,11 @@ namespace CompanyManager
     {
         public override void CalculateSalary()
         {
-            throw new NotImplementedException();
+            const int quarterlyBonus = 800;
+            const double hourly = 20;
+            double salaryAnnualy = 2 * hourly + 3 * quarterlyBonus + 1000;
+            
+
         }
     }
 
@@ -34,7 +43,10 @@ namespace CompanyManager
     {
         public override void CalculateSalary()
         {
-            throw new NotImplementedException();
+            const int quarterlyBonus = 1000;
+            const double departmentComission = quarterlyBonus * 2.3;
+            const double hourly = 20;
+            double salaryAnnualy = 2 * hourly + 3 * quarterlyBonus + 1000;
         }
     }
 
@@ -42,7 +54,10 @@ namespace CompanyManager
     {
         public override void CalculateSalary()
         {
-            throw new NotImplementedException();
+            const int quarterlyBonus = 5000;
+            const double departmentComission = quarterlyBonus * 7.8;
+            const double hourly = 38;
+            double salaryAnnualy = 2 * hourly + 3 * quarterlyBonus + 1000;
         }
     }
 }
