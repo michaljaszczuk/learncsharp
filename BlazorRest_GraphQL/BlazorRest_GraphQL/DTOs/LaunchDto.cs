e44e201fefb383571f7a0f608ecfd3568b027972
michaljaszczuk <michaljaszczuk@gmail.com>
2021-05-16 14:53:09 +0200

    Blazor project repo added

---
+++ BlazorRest_GraphQL/BlazorRest_GraphQL/DTOs/LaunchDto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace BlazorRest_GraphQL.DTOs
+{
+    public partial class LaunchDto
+    {
+        [JsonPropertyName("id")]        
+        public string Id { get; set; }
+
+        [JsonPropertyName("is_tentative")]
+        public bool IsTentative { get; set; }
+
+        [JsonPropertyName("launch_date_local")]
+        public DateTimeOffset LaunchDateLocal { get; set; }
+
+        [JsonPropertyName("mission_name")]
+        public string MissionName { get; set; }
+    }
+}
---
+++ BlazorRest_GraphQL/BlazorRest_GraphQL/Pages/FetchData.cs
@@ -0,0 +1,19 @@
+﻿using BlazorRest_GraphQL.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+namespace BlazorRest_GraphQL.Pages
+{
+    public partial class FetchData
+    {
+        private LaunchDto[] launches;
+
+        protected override async Task OnInitializedAsync()
+        {
+            launches = await Http.GetFromJsonAsync<LaunchDto[]>("https://api.spacex.land/rest/launches/");
+        }
+    }
+}
