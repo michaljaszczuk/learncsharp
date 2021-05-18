8307810f465ccba8420a8fc5cc13481a07709ff0
michaljaszczuk <michaljaszczuk@gmail.com>
2021-05-18 21:07:57 +0200

    Food.cs changed

--- GraphQL_API/GraphQL_API/Models/Food.cs
+++ GraphQL_API/GraphQL_API/Models/Food.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,11 +8,13 @@ namespace GraphQL_API.Models
 {
     public class Food
     {
+        [Key]
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
         public string Group { get; set; }
         public int Calories { get; set; }
-        public double Protein { get; set; }
+        public double Proteins { get; set; }
         public double Carbs { get; set; }
     }
 }
