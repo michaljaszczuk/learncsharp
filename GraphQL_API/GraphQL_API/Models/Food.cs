a59671907c73561994b32b7dbcc3f062b767c9dd
michaljaszczuk <michaljaszczuk@gmail.com>
2021-05-18 20:51:53 +0200

    needed packages installed | Food.cs model added

---
+++ GraphQL_API/GraphQL_API/Models/Food.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GraphQL_API.Models
+{
+    public class Food
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Group { get; set; }
+        public int Calories { get; set; }
+        public double Protein { get; set; }
+        public double Carbs { get; set; }
+    }
+}
--- GraphQL_API/GraphQL_API/Startup.cs
+++ GraphQL_API/GraphQL_API/Startup.cs
@@ -1,10 +1,12 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -12,6 +14,12 @@ namespace GraphQL_API
 {
     public class Startup
     {
+        private readonly IConfiguration Configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
